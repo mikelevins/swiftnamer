53b4e77fbb877f81cba4e9f312ea08f9cf075745
mikel evins <mevins@me.com>
2020-06-30 18:51:28 -0500

    added code to get args

--- swiftnamer/main.swift
+++ swiftnamer/main.swift
@@ -8,5 +8,16 @@
 
 import Foundation
 
-print("Hello, World!")
+func readlines(_ pathname: String) -> [String] {
+    let contents = try! String(contentsOfFile: pathname)
+    let lines = contents.components(separatedBy: "\n")
+    return lines
+}
 
+
+let args = CommandLine.arguments
+let lines = readlines("/Users/mikel/Workshop/src/swiftnamer/data/gnome.names")
+for line in lines {
+    print("\(line)")
+}
+print("Command line arguments: ", args)
