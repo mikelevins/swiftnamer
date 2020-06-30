cff64ae0da7702c2d0413d91fbc750c346fd638a
mikel evins <mevins@me.com>
2020-06-30 17:09:25 -0500

    Initial Commit

---
+++ swiftnamer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftnamer
+//
+//  Created by mikel evins on 6/30/20.
+//  Copyright © 2020 mikel evins. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
