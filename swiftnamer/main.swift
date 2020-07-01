1efd6fe923a0c44adacbe7ec2bef766483b3a72e
mikel evins <mevins@me.com>
2020-06-30 19:52:22 -0500

    adding more namer utils

--- swiftnamer/main.swift
+++ swiftnamer/main.swift
@@ -11,13 +11,37 @@ import Foundation
 func readlines(_ pathname: String) -> [String] {
     let contents = try! String(contentsOfFile: pathname)
     let lines = contents.components(separatedBy: "\n")
-    return lines
+    let filtered_lines = lines.filter { $0.count > 2 }
+    return filtered_lines
 }
 
+func triple(from: String, at: Int) -> String {
+    let startIndex = from.index(from.startIndex, offsetBy: at)
+    let endIndex = from.index(from.startIndex, offsetBy: at+3)
+    let range = Range(uncheckedBounds: (lower: startIndex, upper: endIndex))
+    let letters = String(from[range])
+    return letters
+}
+
+func triples(from: String) -> [String] {
+    let charcount = from.count
+    let subrange = 0..<(charcount-2)
+    return subrange.map {
+        triple(from: from, at: $0)
+    }
+}
+
+func starts(_ trips: [[String]]) -> [String] {
+    return trips.map { $0[0] }
+}
 
 let args = CommandLine.arguments
-let lines = readlines("/Users/mikel/Workshop/src/swiftnamer/data/gnome.names")
-for line in lines {
-    print("\(line)")
+let samples_path = args[1]
+let sample_lines = readlines(samples_path)
+let sample_triples = sample_lines.map { triples(from: $0) }
+let sample_starts = sample_triples.map { $0[0] }
+let sample_parts = sample_triples.map { Array($0.dropFirst()) }
+
+for parts in sample_parts {
+    print("\(parts)")
 }
-print("Command line arguments: ", args)
